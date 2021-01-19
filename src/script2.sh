b52349ddb7dacaab3e82f6e1001af1b448695c5d
mariabazotte <mariabazotte@gmail.com>
2021-01-19 09:59:45 +0100

    new script sh

---
+++ src/script2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+ 
+#===============================================================================
+# exemples d'options
+ 
+#SBATCH --partition=normal        # choix de la partition où soumettre le job
+#SBATCH --nodes=1
+#SBATCH --nodelist=node22         # choix du noeud
+#SBATCH --ntasks=1                # nombre processus
+#SBATCH --mem=8000                # mémoire nécessaire (par noeud) en Mo
+ 
+#===============================================================================
+#exécution du programme (remplacer exe par le nom du programme
+# ou la ligne de commande à exécuter)
+./exe teste3.txt
